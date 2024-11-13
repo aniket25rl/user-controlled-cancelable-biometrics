126bd4b8e279eff259da5c3ec99a6d141736e7ad
Rudraksh Naik <rudrakshnaik8990@gmail.com>
2024-11-13 01:13:59 -0800

    Refactored file_processing.py Added comments

--- src/data_processing/file_processing.py
+++ src/data_processing/file_processing.py
@@ -10,30 +10,44 @@ from pathlib import Path
 BASE_DIR = Path().resolve()
 print(BASE_DIR)
 
-DATASET1 = r"dataset/DB1_A"
-DATASET2 = r"dataset/NEW_DB"
-os.chdir(DATASET1)
+NEW_DATASET_PATH = r"dataset/NEW_DB"
 
-try : 
-    # Create folder NEW_DB
-    os.mkdir(os.path.join(BASE_DIR , DATASET2))
+def organize_user_files(src : str) :
+    """
+    Used to create new dataset from the existing dataset with organized user files.
 
-except FileExistsError :
-    pass
+    Parameters : 
+        src (str) - Path of existing dataset
 
+    
+    """
 
-for filename in os.listdir() : 
+    os.chdir(src)
 
-    user , template = filename.split("_")
-    user_dir_path = os.path.join(BASE_DIR , DATASET2 , user)
-
-    try :     
-        # Create user folder 
-        os.mkdir(user_dir_path)
+    try : 
+        # Create folder NEW_DB
+        os.mkdir(os.path.join(BASE_DIR , NEW_DATASET_PATH))
 
     except FileExistsError :
         pass
 
-    finally :
-        # Move the file to user folder
-        shutil.move(filename ,  user_dir_path)
+
+    for filename in os.listdir() : 
+
+        user , template = filename.split("_")
+        user_dir_path = os.path.join(BASE_DIR , NEW_DATASET_PATH , user)
+
+        try :     
+            # Create user folder 
+            os.mkdir(user_dir_path)
+
+        except FileExistsError :
+            pass
+
+        finally :
+            # Move the file to user folder
+            shutil.move(filename ,  user_dir_path)
+
+if __name__ == "__main__" :
+    DATASET1 = r"dataset/DB1_A"
+    organize_user_files(DATASET1)
